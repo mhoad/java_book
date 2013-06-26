07dffff3db286fcb29fe80ec16199a5de46c10d6
Mark Hoad <markdhoad@gmail.com>
2013-06-26 14:20:21 +0800

    Chapter 2 Exercise 2

---
+++ Chapter 2/exercise1.java
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ * Java Programming - From Problem Analysis to Program Design 5th Edition
+ * Chapter 2 - Exercise 1
+ * markdhoad@gmail.com
+ * 
+ * Write a program that produces the following output:
+ * 
+ * **********************************
+ * *    Programming Assignment 1    *
+ * *     Computer Programming 1     *
+ * *       Author: Daffy Duck       *
+ * *   Due Date: Thursday, Jan 24   *
+ * **********************************
+ * 
+ *****************************************************************************/
+
+public class exercise1 {
+   public static void main(String[] args){
+       System.out.println("**********************************");
+       System.out.println("*    Programming Assignment 1    *");
+       System.out.println("*     Computer Programming 1     *");
+       System.out.println("*       Author: Daffy Duck       *");
+       System.out.println("*   Due Date: Thursday, Jan 24   *");
+       System.out.println("**********************************");
+   } 
+}
---
+++ Chapter 2/exercise2.java
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Java Programming - From Problem Analysis to Program Design 5th Edition
+ * Chapter 2 - Exercise 1
+ * markdhoad@gmail.com
+ * 
+ * 1. Write Java statements that declare the following variables: 
+ *    num1, num2, and num3, and average of type int.
+ * 2. Write Java statements that store 125 into num1, 28 into num2, 
+ *    and -25 into num3.
+ * 3. Write a Java statement that stores the average of num1, num2, 
+ *    and num3 into average.
+ * 4. Write Java statements that output the values of num1, num2, 
+ *    num3, and average.
+ * 5. Compile and run your program.
+ *****************************************************************************/
+
+public class exercise2 {
+   public static void main(String[] args){
+       int num1, num2, num3, average;
+       num1 = 125;
+       num2 = 28;
+       num3 = -25;
+       average = (num1 + num2 + num3) / 3;
+
+       System.out.println("Num 1 = " + num1);
+       System.out.println("Num 2 = " + num2);
+       System.out.println("Num 3 = " + num3);
+       System.out.println("Average = " + average);
+   } 
+}
